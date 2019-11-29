80b578d35ef7f31e42dc23823d5e5dce7e1eab73
Soma Halasi <somahal18@gmail.com>
2019-11-29 17:31:16 +0100

    törlési funkció

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -54,5 +54,10 @@ namespace UserMaintenance
                 }
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            users.Remove((User)listBox1.SelectedItem);
+        }
     }
 }
